f1143d5fa4e2e6237f3d43121d0d1cf16c2f08b5
Kevin Meredith <kmeredith@disne.lab>
2021-02-19 16:31:27 -0500

    Add project files.

---
+++ ssl-config/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ssl_config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string result = ExecuteCmd.Run("netsh http show sslcert");
+
+
+            List<string> lines = result.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+
+            List<string> list;
+            Dictionary<string, List<string>> entries = new Dictionary<string, List<string>>();
+            
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("Press any key to close.");
+            Console.ReadLine();
+        }
+    }
+}
